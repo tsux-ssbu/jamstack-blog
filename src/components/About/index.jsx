8a396c363ab2e4344e8773f14dead070c6e4a235
tsux-ssbu <tomoya18xxx@gmail.com>
2022-01-27 11:57:40 +0900

    🎨 Works, Aboutをいい感じにした

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,13 +1,15 @@
 export const About = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h2 className="self-start md:ml-16">About me</h2>
-      <ul>
-        <li>岡山大学経済学部生(23卒)</li>
-        <li>1年くらいプログラミングを学んでいます</li>
-        <li>フロントエンド開発が好きで、モバイル開発にも興味があります</li>
-        <li>CS50でコンピューターサイエンスを学びました</li>
-      </ul>
+    <div className="flex flex-col justify-center items-center mt-8">
+      <h2 className="self-start ml-0 md:ml-16">About me</h2>
+      <div className="mt-4 w-11/12 md:w-8/12">
+        <ul>
+          <li>岡山大学経済学部生(23卒)</li>
+          <li>1年くらいプログラミングを学んでいます</li>
+          <li>CS50でコンピューターサイエンスを学びました</li>
+          <li>フロントエンド開発が好きです</li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -1,18 +1,85 @@
+import Link from "next/link";
+
+const PRODUCT_ITEMS = [
+  {
+    title: "20.Timer",
+    discription:
+      "たいていのことは20時間学習したら習得できるという20時間の法則を実践することを支援するシンプルなカウントダウンタイマー+メモアプリです。",
+    url: "https://twenty-hours-timer.com/",
+    zenn: "https://zenn.dev/tsuxxx/articles/03bdd626266d13",
+    github: "https://github.com/tsux-ssbu/supatimer",
+    lang: ["Next.js", "Supabase", "Chakra UI"],
+  },
+  {
+    title: "5minutes todo app",
+    discription:
+      "5分で終わるような簡単なタスクだけを登録し、スタートしたら完了するか５分たつと勝手にタスクが消えるTodoモバイルアプリです。怠惰な自分のためにつくりました。",
+    url: "https://apps.apple.com/jp/app/5minutes-todo/id1598084813",
+    zenn: "https://zenn.dev/tsuxxx/articles/3b2bd7ca9ba631",
+    github: "https://github.com/tsux-ssbu/5minutes",
+    lang: ["React Native", "Firebase"],
+  },
+  {
+    title: "都道府県別の総人口推移グラフ",
+    discription:
+      "都道府県別人口推移を表示します。とある企業の選考課題でつくった。外部APIやチャートライブラリを使うのは初めてだったので勉強になったです。(選考は無事落ちちゃいました)",
+    url: "https://population-transition-chart-tsux.vercel.app/",
+    zenn: undefined,
+    github: "https://github.com/tsux-ssbu/yumemi-task",
+    lang: ["Next.js", "highcharts"],
+  },
+];
+
 export const Works = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center mt-8">
       <h2 className="self-start md:ml-16">Works</h2>
-      <div>
-        <h3>20.Timer</h3>
-        <p>20時間の法則どうこう</p>
-      </div>
-      <div>
-        <h3>5minutes</h3>
-        <p>5分頑張ろう的な</p>
-      </div>
-      <div>
-        <h3>人口推移</h3>
-        <p>企業の選考課題で作成</p>
+      <div className="w-11/12 md:w-8/12">
+        {PRODUCT_ITEMS.map((product) => {
+          return (
+            <div key={product.title} className="mt-4 ">
+              <h3 className="text-lg font-semibold">{product.title}</h3>
+              <p className="">{product.discription}</p>
+              <span>
+                使用技術：
+                {product.lang.map((item) => {
+                  return item + "  ";
+                })}
+              </span>
+
+              <div>
+                <Link href={product.url}>
+                  <a
+                    target="_blank"
+                    className="mr-4 text-blue-500 hover:underline"
+                  >
+                    サービスURL↗
+                  </a>
+                </Link>
+                {product.zenn ? (
+                  <Link href={product.zenn}>
+                    <a
+                      target="_blank"
+                      className="mr-4 text-blue-500 hover:underline"
+                    >
+                      Zenn↗
+                    </a>
+                  </Link>
+                ) : undefined}
+                {product.github ? (
+                  <Link href={product.github}>
+                    <a
+                      target="_blank"
+                      className="text-blue-500 hover:underline"
+                    >
+                      github↗
+                    </a>
+                  </Link>
+                ) : undefined}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
