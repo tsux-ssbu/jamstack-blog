dd9c0b5a448d3f725b54b01f441c45b41e23baf0
tsux-ssbu <tomoya18xxx@gmail.com>
2022-02-23 06:11:11 +0900

    📈 umami

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -18,6 +18,12 @@ function MyApp({ Component, pageProps }) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🦔</text></svg>"
         ></link>
+        <script
+          async
+          defer
+          data-website-id="f7d63978-2740-4b75-951b-e3068885f9ac"
+          src="https://my-umami-swart.vercel.app/umami.js"
+        ></script>
       </Head>
       <Layout>
         <Header />
