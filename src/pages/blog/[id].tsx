da4d30763c22156f8838c6a010337a70d2e38812
tsux-ssbu <tomoya18xxx@gmail.com>
2022-04-03 13:21:09 +0900

    refactor: ブログ詳細画面をTypeScript化

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,6 +1,13 @@
-import { client } from "../../libs/client";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 
-export default function BlogId({ blog }) {
+import { client } from "../../libs/client";
+import { BlogType } from "../../type/blog";
+
+type Props = {
+  blog: BlogType;
+};
+
+const BlogId: NextPage<Props> = ({ blog }) => {
   return (
     <main className="mx-auto mt-12 w-11/12 md:w-10/12">
       <h1>{blog.title}</h1>
@@ -13,24 +20,34 @@ export default function BlogId({ blog }) {
       />
     </main>
   );
-}
+};
 
 // 静的生成のためのパスを指定
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths<
+  Pick<BlogType, "id">
+> = async () => {
   const data = await client.get({ endpoint: "blog" });
-
-  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  const paths = data.contents.map((content: any) => `/blog/${content.id}`);
   return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
+export const getStaticProps: GetStaticProps<
+  Props,
+  Pick<BlogType, "id">
+> = async ({ params }) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+  const id = params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
-
   return {
     props: {
       blog: data,
     },
   };
 };
+
+export default BlogId;
