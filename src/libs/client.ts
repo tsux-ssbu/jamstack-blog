9bf611f495ef01bc55273466243dc68e4cd905b5
tsux-ssbu <tomoya18xxx@gmail.com>
2022-04-03 13:22:44 +0900

    change: tsxファイルに変更

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "tsuka6log",
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY || "",
 });
