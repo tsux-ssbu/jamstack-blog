8305b681b1e63bdfb84b6b42c4c82a8e222acb28
tsux-ssbu <tomoya18xxx@gmail.com>
2022-04-18 03:12:32 +0900

    refactor: 型にMicroCMSListResponseを利用

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,22 +1,27 @@
 import { GetStaticProps, NextPage } from 'next'
 import Link from 'next/link'
 
+import { MicroCMSListResponse } from 'microcms-js-sdk'
+
 import { BlogType } from '@/type/blog'
 import { client } from '@/libs/client'
 
-type Props = {
-  allBlogsData: BlogType[]
+type Blog = {
+  title: string
+  body: string
 }
 
-const Blog: NextPage<Props> = ({ allBlogsData }) => {
+type Props = MicroCMSListResponse<Blog>
+
+const Blog: NextPage<Props> = (props) => {
   return (
     <main className='mt-12 h-screen'>
       <div className='mx-auto w-11/12 md:w-10/12'>
         <ul>
-          {allBlogsData.map((blog: BlogType) => (
-            <li key={blog.id} className='mt-8'>
-              <Link href={`/blog/${blog.id}`}>
-                <a className='text-2xl font-bold hover:underline'>{blog.title}</a>
+          {props.contents.map((content: BlogType) => (
+            <li key={content.id} className='mt-8'>
+              <Link href={`/blog/${content.id}`}>
+                <a className='text-2xl font-bold hover:underline'>{content.title}</a>
               </Link>
               {/* <div className="flex items-center">
                 <p className="mr-8">時間が入る</p>
@@ -30,11 +35,9 @@ const Blog: NextPage<Props> = ({ allBlogsData }) => {
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const allBlogsData = await client.getList({ endpoint: 'blog' })
+  const data = await client.getList({ endpoint: 'blog' })
   return {
-    props: {
-      allBlogsData: allBlogsData.contents,
-    },
+    props: data,
   }
 }
 
